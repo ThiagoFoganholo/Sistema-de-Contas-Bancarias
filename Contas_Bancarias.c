a2a66e04128c2f5309039b826e13ebb62aafd5d3
thiagofoganholo <you@example.com>
2023-11-27 09:38:12 -0300

    initial commit

---
+++ Contas_Bancarias.c
@@ -0,0 +1,5 @@
+#include "Contas_Bancarias.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Contas_Bancarias.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
